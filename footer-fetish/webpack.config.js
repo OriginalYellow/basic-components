8e580a8c84e631c5641562a3dd4d41495f476a90
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-10-16 11:37:36 -0400

    basic webpack setup

---
+++ footer-fetish/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const REMOVE_STYLES_JS = 'RemoveStylesJSPlugin'
+outputPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  target: 'web',
+  output: {
+    filename: '[name].bundle.js',
+    path: outputPath,
+  },
+  entry: {
+    global: './src/global.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+        ]
+      },
+    ],
+  },
+  watch: true,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap(REMOVE_STYLES_JS, compilation => {
+          try {
+            fs.unlinkSync(path.join('./dist', 'global.bundle.js'))
+          } catch (e) {
+            compilation.getLogger(REMOVE_STYLES_JS).warn(`${REMOVE_STYLES_JS} failed`)
+          }
+        });
+      }
+    },
+  ],
+  devServer: {
+    publicPath: '/',
+    contentBase: outputPath,
+    watchContentBase: true,
+    compress: true,
+    port: 9001
+  },
+}
