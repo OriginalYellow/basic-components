cf170330eceaaafb7bcf293fb2f24fd3c9e89122
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-10-16 14:30:25 -0400

    set up vercel deployment

--- footer-fetish/webpack.config.js
+++ footer-fetish/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const REMOVE_STYLES_JS = 'RemoveStylesJSPlugin'
-outputPath = path.resolve(__dirname, 'dist');
+outputPath = path.resolve(__dirname, 'public');
 
 module.exports = {
   target: 'web',
@@ -27,7 +27,6 @@ module.exports = {
       },
     ],
   },
-  watch: true,
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
